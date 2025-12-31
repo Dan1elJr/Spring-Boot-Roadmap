b8157cfb050c6c8eb8616d0685b230a53c8b271c
Daniel Junior <danieljunior1903@gmail.com>
2025-12-31 17:30:50 -0400

    Spring REST/CRUD - Get a single employee (by id)

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/sandaniel/springboot/demo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/sandaniel/springboot/demo/rest/EmployeeRestController.java
@@ -1,9 +1,13 @@
 package com.sandaniel.springboot.demo.rest;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +36,16 @@ public class EmployeeRestController {
 		
 	}
 	
+	@GetMapping("employees/{theEmployeeId}")
+	public Employee findEmployee (@PathVariable int theEmployeeId ) {
+		
+		// Retrieving the employee data with nullable check
+		Employee theEmployee = Optional.ofNullable(employeeService.findById(theEmployeeId))
+				.orElseThrow(() -> new RuntimeException("Employee id not found - " +theEmployeeId));
+	
+		
+		
+		return theEmployee;
+	}
 	
 }
