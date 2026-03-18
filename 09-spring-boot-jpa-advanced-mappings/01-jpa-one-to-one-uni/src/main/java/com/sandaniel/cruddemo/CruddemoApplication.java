8bcb70bab82e8b2811d004a4e1119e20e9555cd1
Daniel Junior <danieljunior1903@gmail.com>
2026-03-18 14:21:53 -0400

    Springboot/JPA/Advanced mapping - implementing 'deleteInstructor byId' Funcionality

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/sandaniel/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/sandaniel/cruddemo/CruddemoApplication.java
@@ -20,11 +20,23 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner (AppDAO appDAO) {
 		
 		return runner -> {
-			createInstructor(appDAO);
-			findInstructor(appDAO);
+			//createInstructor(appDAO);
+			//findInstructor(appDAO);
+			deleteInstructor(appDAO);
 		};
 	}
 	
+	private void deleteInstructor(AppDAO appDAO) {
+		
+		int theId = 1;
+		System.out.println("Deleting instructor id: "+theId);
+		
+		appDAO.deleteInstructorById(theId);
+		
+		System.out.println("Done!");
+		
+	}
+
 	private void findInstructor(AppDAO appDAO) {
 		int theId = 1;
 		System.out.println("Finding instructor id: "+theId);
--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/sandaniel/cruddemo/dao/AppDAO.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/sandaniel/cruddemo/dao/AppDAO.java
@@ -5,6 +5,9 @@ import com.sandaniel.cruddemo.entity.Instructor;
 public interface AppDAO {
 	
 	void save (Instructor theInstructor);
+	
 	Instructor findInstructorById(int theId);
+	
+	void deleteInstructorById(int theId);
 	
 }
